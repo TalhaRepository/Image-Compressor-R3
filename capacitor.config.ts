84ee668e1063e59111456066cf186a39597f9d04
TalhaRepository <thehutree@gmail.com>
2026-08-21 14:26:35 +0500

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.brivaltop.imagecompressor.resizer.mbtokb',
+  appName: 'Image Compressor & Resizer in KB',
+  webDir: 'dist',
+};
+
+export default config;
